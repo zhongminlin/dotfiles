a43df7dbc8e5556945849c70a890cfec9d588e26
Zhongmin Lin <zhongmin.lin3@gmail.com>
2024-06-17 12:26:38 -0400

    Initial commit

---
+++ dot_config/private_qutebrowser/config.py
@@ -0,0 +1,295 @@
+import colors
+
+colors = colors.morandi_dark
+
+config.load_autoconfig()
+
+# dark mode
+c.colors.webpage.darkmode.enabled = True
+
+# clipboard
+c.content.javascript.clipboard = 'access-paste'
+
+# fonts
+# c.fonts.default_family = [ "CaskaydiaCove Nerd Font", "Noto Color Emoji" ]
+# c.fonts.default_size = "8pt"
+
+# set qutebrowser colors
+
+# Text color of the completion widget. May be a single color to use for
+# all columns or a list of three colors, one for each column.
+c.colors.completion.fg = colors[5]
+
+# Background color of the completion widget for odd rows.
+c.colors.completion.odd.bg = colors[1]
+
+# Background color of the completion widget for even rows.
+c.colors.completion.even.bg = colors[0]
+
+# Foreground color of completion widget category headers.
+c.colors.completion.category.fg = colors[10]
+
+# Background color of the completion widget category headers.
+c.colors.completion.category.bg = colors[0]
+
+# Top border color of the completion widget category headers.
+c.colors.completion.category.border.top = colors[0]
+
+# Bottom border color of the completion widget category headers.
+c.colors.completion.category.border.bottom = colors[0]
+
+# Foreground color of the selected completion item.
+c.colors.completion.item.selected.fg = colors[5]
+
+# Background color of the selected completion item.
+c.colors.completion.item.selected.bg = colors[2]
+
+# Top border color of the selected completion item.
+c.colors.completion.item.selected.border.top = colors[2]
+
+# Bottom border color of the selected completion item.
+c.colors.completion.item.selected.border.bottom = colors[2]
+
+# Foreground color of the matched text in the selected completion item.
+c.colors.completion.item.selected.match.fg = colors[11]
+
+# Foreground color of the matched text in the completion.
+c.colors.completion.match.fg = colors[11]
+
+# Color of the scrollbar handle in the completion view.
+c.colors.completion.scrollbar.fg = colors[5]
+
+# Color of the scrollbar in the completion view.
+c.colors.completion.scrollbar.bg = colors[0]
+
+# Background color of disabled items in the context menu.
+c.colors.contextmenu.disabled.bg = colors[1]
+
+# Foreground color of disabled items in the context menu.
+c.colors.contextmenu.disabled.fg = colors[4]
+
+# Background color of the context menu. If set to null, the Qt default is used.
+c.colors.contextmenu.menu.bg = colors[0]
+
+# Foreground color of the context menu. If set to null, the Qt default is used.
+c.colors.contextmenu.menu.fg =  colors[5]
+
+# Background color of the context menu’s selected item. If set to null, the Qt default is used.
+c.colors.contextmenu.selected.bg = colors[2]
+
+#Foreground color of the context menu’s selected item. If set to null, the Qt default is used.
+c.colors.contextmenu.selected.fg = colors[5]
+
+# Background color for the download bar.
+c.colors.downloads.bar.bg = colors[0]
+
+# Color gradient start for download text.
+c.colors.downloads.start.fg = colors[0]
+
+# Color gradient start for download backgrounds.
+c.colors.downloads.start.bg = colors[13]
+
+# Color gradient end for download text.
+c.colors.downloads.stop.fg = colors[0]
+
+# Color gradient stop for download backgrounds.
+c.colors.downloads.stop.bg = colors[12]
+
+# Foreground color for downloads with errors.
+c.colors.downloads.error.fg = colors[8]
+
+# Font color for hints.
+c.colors.hints.fg = colors[0]
+
+# Background color for hints. Note that you can use a `rgba(...)` value
+# for transparency.
+c.colors.hints.bg = colors[10]
+
+# Font color for the matched part of hints.
+c.colors.hints.match.fg = colors[5]
+
+# Text color for the keyhint widget.
+c.colors.keyhint.fg = colors[5]
+
+# Highlight color for keys to complete the current keychain.
+c.colors.keyhint.suffix.fg = colors[5]
+
+# Background color of the keyhint widget.
+c.colors.keyhint.bg = colors[0]
+
+# Foreground color of an error message.
+c.colors.messages.error.fg = colors[0]
+
+# Background color of an error message.
+c.colors.messages.error.bg = colors[8]
+
+# Border color of an error message.
+c.colors.messages.error.border = colors[8]
+
+# Foreground color of a warning message.
+c.colors.messages.warning.fg = colors[0]
+
+# Background color of a warning message.
+c.colors.messages.warning.bg = colors[14]
+
+# Border color of a warning message.
+c.colors.messages.warning.border = colors[14]
+
+# Foreground color of an info message.
+c.colors.messages.info.fg = colors[5]
+
+# Background color of an info message.
+c.colors.messages.info.bg = colors[0]
+
+# Border color of an info message.
+c.colors.messages.info.border = colors[0]
+
+# Foreground color for prompts.
+c.colors.prompts.fg = colors[5]
+
+# Border used around UI elements in prompts.
+c.colors.prompts.border = colors[0]
+
+# Background color for prompts.
+c.colors.prompts.bg = colors[0]
+
+# Background color for the selected item in filename prompts.
+c.colors.prompts.selected.bg = colors[2]
+
+# Foreground color for the selected item in filename prompts.
+c.colors.prompts.selected.fg = colors[5]
+
+# Foreground color of the statusbar.
+c.colors.statusbar.normal.fg = colors[11]
+
+# Background color of the statusbar.
+c.colors.statusbar.normal.bg = colors[0]
+
+# Foreground color of the statusbar in insert mode.
+c.colors.statusbar.insert.fg = colors[0]
+
+# Background color of the statusbar in insert mode.
+c.colors.statusbar.insert.bg = colors[13]
+
+# Foreground color of the statusbar in passthrough mode.
+c.colors.statusbar.passthrough.fg = colors[0]
+
+# Background color of the statusbar in passthrough mode.
+c.colors.statusbar.passthrough.bg = colors[12]
+
+# Foreground color of the statusbar in private browsing mode.
+c.colors.statusbar.private.fg = colors[0]
+
+# Background color of the statusbar in private browsing mode.
+c.colors.statusbar.private.bg = colors[1]
+
+# Foreground color of the statusbar in command mode.
+c.colors.statusbar.command.fg = colors[5]
+
+# Background color of the statusbar in command mode.
+c.colors.statusbar.command.bg = colors[0]
+
+# Foreground color of the statusbar in private browsing + command mode.
+c.colors.statusbar.command.private.fg = colors[5]
+
+# Background color of the statusbar in private browsing + command mode.
+c.colors.statusbar.command.private.bg = colors[0]
+
+# Foreground color of the statusbar in caret mode.
+c.colors.statusbar.caret.fg = colors[0]
+
+# Background color of the statusbar in caret mode.
+c.colors.statusbar.caret.bg = colors[14]
+
+# Foreground color of the statusbar in caret mode with a selection.
+c.colors.statusbar.caret.selection.fg = colors[0]
+
+# Background color of the statusbar in caret mode with a selection.
+c.colors.statusbar.caret.selection.bg = colors[13]
+
+# Background color of the progress bar.
+c.colors.statusbar.progress.bg = colors[13]
+
+# Default foreground color of the URL in the statusbar.
+c.colors.statusbar.url.fg = colors[5]
+
+# Foreground color of the URL in the statusbar on error.
+c.colors.statusbar.url.error.fg = colors[8]
+
+# Foreground color of the URL in the statusbar for hovered links.
+c.colors.statusbar.url.hover.fg = colors[5]
+
+# Foreground color of the URL in the statusbar on successful load
+# (http).
+c.colors.statusbar.url.success.http.fg = colors[12]
+
+# Foreground color of the URL in the statusbar on successful load
+# (https).
+c.colors.statusbar.url.success.https.fg = colors[11]
+
+# Foreground color of the URL in the statusbar when there's a warning.
+c.colors.statusbar.url.warn.fg = colors[14]
+
+# Background color of the tab bar.
+c.colors.tabs.bar.bg = colors[0]
+
+# Color gradient start for the tab indicator.
+c.colors.tabs.indicator.start = colors[13]
+
+# Color gradient end for the tab indicator.
+c.colors.tabs.indicator.stop = colors[12]
+
+# Color for the tab indicator on errors.
+c.colors.tabs.indicator.error = colors[8]
+
+# Foreground color of unselected odd tabs.
+c.colors.tabs.odd.fg = colors[5]
+
+# Background color of unselected odd tabs.
+c.colors.tabs.odd.bg = colors[1]
+
+# Foreground color of unselected even tabs.
+c.colors.tabs.even.fg = colors[5]
+
+# Background color of unselected even tabs.
+c.colors.tabs.even.bg = colors[0]
+
+# Background color of pinned unselected even tabs.
+c.colors.tabs.pinned.even.bg = colors[12]
+
+# Foreground color of pinned unselected even tabs.
+c.colors.tabs.pinned.even.fg = colors[7]
+
+# Background color of pinned unselected odd tabs.
+c.colors.tabs.pinned.odd.bg = colors[11]
+
+# Foreground color of pinned unselected odd tabs.
+c.colors.tabs.pinned.odd.fg = colors[7]
+
+# Background color of pinned selected even tabs.
+c.colors.tabs.pinned.selected.even.bg = colors[2]
+
+# Foreground color of pinned selected even tabs.
+c.colors.tabs.pinned.selected.even.fg = colors[5]
+
+# Background color of pinned selected odd tabs.
+c.colors.tabs.pinned.selected.odd.bg = colors[2]
+
+# Foreground color of pinned selected odd tabs.
+c.colors.tabs.pinned.selected.odd.fg = colors[5]
+
+# Foreground color of selected odd tabs.
+c.colors.tabs.selected.odd.fg = colors[5]
+
+# Background color of selected odd tabs.
+c.colors.tabs.selected.odd.bg = colors[2]
+
+# Foreground color of selected even tabs.
+c.colors.tabs.selected.even.fg = colors[5]
+
+# Background color of selected even tabs.
+c.colors.tabs.selected.even.bg = colors[2]
+
+# Background color for webpages if unset (or empty to use the theme's
+# color).
+# c.colors.webpage.bg = colors[0]
---
+++ dot_config/qtile/config.py
@@ -0,0 +1,316 @@
+# Copyright (c) 2010 Aldo Cortesi
+# Copyright (c) 2010, 2014 dequis
+# Copyright (c) 2012 Randall Ma
+# Copyright (c) 2012-2014 Tycho Andersen
+# Copyright (c) 2012 Craig Barnes
+# Copyright (c) 2013 horsik
+# Copyright (c) 2013 Tao Sauvage
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import os, subprocess
+from libqtile import bar, layout, qtile, widget, extension, hook
+from libqtile.config import Click, Drag, Group, Key, Match, Screen
+from libqtile.lazy import lazy
+from libqtile.utils import guess_terminal
+
+import colors
+
+colors = colors.morandi_dark
+
+mod = "mod4"
+terminal = guess_terminal()
+browser = 'qutebrowser'
+fm = 'thunar'
+
+dmenu_theme = {
+    "background" : colors[0],
+    "foreground" : colors[7],
+    "selected_background" : colors[15],
+    "selected_foreground" : colors[7],
+}
+
+keys = [
+    # A list of available commands that can be bound to keys can be found
+    # at https://docs.qtile.org/en/latest/manual/config/lazy.html
+    # Switch between windows
+    Key([mod], "h", lazy.layout.left(), desc="Move focus to left"),
+    Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
+    Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
+    Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
+    Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
+    # Move windows between left/right columns or move up/down in current stack.
+    # Moving out of range in Columns layout will create new column.
+    Key([mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"),
+    Key([mod, "shift"], "l", lazy.layout.shuffle_right(), desc="Move window to the right"),
+    Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
+    Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
+    # Grow windows. If current window is on the edge of screen and direction
+    # will be to screen edge - window would shrink.
+    Key([mod, "control"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
+    Key([mod, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"),
+    Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
+    Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
+    Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
+    # Toggle between split and unsplit sides of stack.
+    # Split = all windows displayed
+    # Unsplit = 1 window displayed, like Max layout, but still with
+    # multiple stack panes
+    Key(
+        [mod, "shift"],
+        "Return",
+        lazy.layout.toggle_split(),
+        desc="Toggle between split and unsplit sides of stack",
+    ),
+    Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
+    # Toggle between different layouts as defined below
+    Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
+    Key([mod], "c", lazy.window.kill(), desc="Kill focused window"),
+    Key(
+        [mod],
+        "f",
+        lazy.window.toggle_fullscreen(),
+        desc="Toggle fullscreen on the focused window",
+    ),
+    Key([mod], "t", lazy.window.toggle_floating(), desc="Toggle floating on the focused window"),
+    Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
+    Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
+    Key([mod], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
+    # run launcher
+    Key([mod], 'd', lazy.run_extension(extension.DmenuRun(
+        **dmenu_theme,
+        # dmenu_prompt=">",
+        # dmenu_font="Andika-8",
+        # dmenu_height=24,  # Only supported by some dmenu forks
+    ))),
+    # Key([mod], "d", lazy.spawn('dmenu_run'), desc='Dmenu run launcher'),
+    # applications
+    Key([mod, "shift"], "w", lazy.spawn(browser), desc='Launch browser'),
+    Key([mod, "shift"], "f", lazy.spawn(fm), desc='Launch file manager'),
+    # audio and brightness
+    Key([], "XF86AudioMute", lazy.spawn("wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle")),
+    Key([], "XF86AudioLowerVolume", lazy.spawn("wpctl set-volume @DEFAULT_AUDIO_SINK@ 10%-")),
+    Key([], "XF86AudioRaiseVolume", lazy.spawn("wpctl set-volume @DEFAULT_AUDIO_SINK@ 10%+")),
+    Key([], "XF86MonBrightnessUp", lazy.spawn("brightnessctl s 10%+")),
+    Key([], "XF86MonBrightnessDown", lazy.spawn("brightnessctl s 10%-")),
+    # power menu as commandset
+    Key([mod], 'x', lazy.run_extension(extension.CommandSet(
+        commands={
+            'Shutdown': 'systemctl poweroff',
+            'Reboot': 'systemctl reboot',
+            'Suspend': 'systemctl suspend',
+            # 'Lock Screen': 'pfetch',
+            'Log Out': 'qtile cmd-obj -f shutdown',
+            # 'Hibernate': 'systemctl hibernate',
+            },
+        # dmenu_bottom = True,
+        **dmenu_theme,
+        ))), 
+
+]
+
+# Add key bindings to switch VTs in Wayland.
+# We can't check qtile.core.name in default config as it is loaded before qtile is started
+# We therefore defer the check until the key binding is run by using .when(func=...)
+for vt in range(1, 8):
+    keys.append(
+        Key(
+            ["control", "mod1"],
+            f"f{vt}",
+            lazy.core.change_vt(vt).when(func=lambda: qtile.core.name == "wayland"),
+            desc=f"Switch to VT{vt}",
+        )
+    )
+
+grouplabel = ["壹", "贰", "叁", "肆", "伍", "陆", "柒", "捌", "玖"]
+groups = [Group(f"{i+1}", label=grouplabel[i]) for i in range(9)]
+# groups = [Group(i) for i in "123456789"]
+
+for i in groups:
+    keys.extend(
+        [
+            # mod + group number = switch to group
+            Key(
+                [mod],
+                i.name,
+                lazy.group[i.name].toscreen(),
+                desc="Switch to group {}".format(i.name),
+            ),
+            # mod + shift + group number = switch to & move focused window to group
+            Key(
+                [mod, "shift"],
+                i.name,
+                lazy.window.togroup(i.name, switch_group=True),
+                desc="Switch to & move focused window to group {}".format(i.name),
+            ),
+            # Or, use below if you prefer not to switch to that group.
+            # # mod + shift + group number = move focused window to group
+            # Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
+            #     desc="move focused window to group {}".format(i.name)),
+        ]
+    )
+
+
+layout_theme = {
+    "border_width": 2,
+    "margin": 5,
+    "border_focus": colors[11],
+    "border_normal": colors[0]
+}
+    
+layouts = [
+    layout.Bsp(**layout_theme,fair=False),
+    layout.MonadTall(**layout_theme),
+    # layout.Tile(**layout_theme),
+    layout.Columns(**layout_theme),
+    layout.Max(),
+    # Try more layouts by unleashing below layouts.
+    # layout.Stack(num_stacks=2),
+    # layout.Matrix(),
+    # layout.MonadWide(),
+    # layout.RatioTile(),
+    # layout.TreeTab(),
+    # layout.VerticalTile(),
+    # layout.Zoomy(),
+]
+
+widget_defaults = dict(
+    font="sans",
+    # font="NotoSansM Nerd Font Mono",
+    fontsize=12,
+    padding=3,
+    foreground=colors[7],
+    background=colors[0],
+)
+extension_defaults = widget_defaults.copy()
+
+screens = [
+    Screen(
+        bottom=bar.Bar(
+            [
+                widget.TextBox("", name="archlinux", padding=5, foreground=colors[13]),
+                widget.Sep(padding=15, size_percent=10),
+                widget.CurrentLayoutIcon(scale=0.5),
+                widget.CurrentLayout(),
+                widget.Sep(padding=15, size_percent=10),
+                widget.GroupBox(
+                    active=colors[7],
+                    inactive=colors[2],
+                    this_current_screen_border=colors[3],
+                    # this_current_border=colors[15],
+                ),
+                widget.Prompt(),
+                widget.WindowName(),
+                widget.Chord(
+                    chords_colors={
+                        "launch": ("#ff0000", "#ffffff"),
+                    },
+                    name_transform=lambda name: name.upper(),
+                ),
+                # widget.TextBox("default config", name="default"),
+                # widget.TextBox("Press &lt;M-r&gt; to spawn", foreground="#d75f5f"),
+                # NB Systray is incompatible with Wayland, consider using StatusNotifier instead
+                # widget.StatusNotifier(),
+                widget.CPU(fmt='  {}', format='{load_percent}%', foreground=colors[15]),
+                widget.Sep(padding=15, size_percent=10),
+                widget.Memory(fmt='  {}',  format='{MemUsed: .1f}{mm}',measure_mem='G',foreground=colors[11]),
+                widget.Sep(padding=15, size_percent=10),
+                widget.DF(visible_on_warn=False,fmt='󰋊  {}',foreground=colors[3]),
+                widget.Sep(padding=15, size_percent=10),
+                widget.Battery(fmt='󰁹  {}',format='{char} {percent:2.0%} {hour:d}:{min:02d}',foreground=colors[9]),
+                widget.Sep(padding=15, size_percent=10),
+                widget.WidgetBox(widgets=[
+                    widget.Backlight(fmt='󰃟  {}',backlight_name='amdgpu_bl0'),
+                    widget.Sep(padding=15, size_percent=10),
+                    widget.PulseVolume(fmt='󰕾  {}'),
+                    widget.Sep(padding=15, size_percent=10),
+                    widget.Bluetooth(fmt='󰂯  {}'),
+                ]),
+                widget.Sep(padding=15, size_percent=10),
+                widget.Systray(),
+                widget.Sep(padding=15, size_percent=10),
+                widget.Clock(format="%Y-%m-%d %a %I:%M %p",foreground=colors[13]),
+                # widget.QuickExit(),
+            ],
+            24,
+            # border_width=[2, 0, 2, 0],  # Draw top and bottom borders
+            # border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
+        ),
+        # You can uncomment this variable if you see that on X11 floating resize/moving is laggy
+        # By default we handle these events delayed to already improve performance, however your system might still be struggling
+        # This variable is set to None (no cap) by default, but you can set it to 60 to indicate that you limit it to 60 events per second
+        # x11_drag_polling_rate = 60,
+    ),
+]
+
+# Drag floating layouts.
+mouse = [
+    Drag([mod], "Button1", lazy.window.set_position_floating(), start=lazy.window.get_position()),
+    Drag([mod], "Button3", lazy.window.set_size_floating(), start=lazy.window.get_size()),
+    Click([mod], "Button2", lazy.window.bring_to_front()),
+]
+
+dgroups_key_binder = None
+dgroups_app_rules = []  # type: list
+follow_mouse_focus = True
+bring_front_click = False
+floats_kept_above = True
+cursor_warp = False
+floating_layout = layout.Floating(
+    float_rules=[
+        # Run the utility of `xprop` to see the wm class and name of an X client.
+        *layout.Floating.default_float_rules,
+        Match(wm_class="confirmreset"),  # gitk
+        Match(wm_class="makebranch"),  # gitk
+        Match(wm_class="maketag"),  # gitk
+        Match(wm_class="ssh-askpass"),  # ssh-askpass
+        Match(title="branchdialog"),  # gitk
+        Match(title="pinentry"),  # GPG key password entry
+    ]
+)
+auto_fullscreen = True
+focus_on_window_activation = "smart"
+reconfigure_screens = True
+
+# If things like steam games want to auto-minimize themselves when losing
+# focus, should we respect this or not?
+auto_minimize = True
+
+# When using the Wayland backend, this can be used to configure input devices.
+wl_input_rules = None
+
+# xcursor theme (string or None) and size (integer) for Wayland backend
+wl_xcursor_theme = None
+wl_xcursor_size = 24
+
+# programs to start with qtile
+@hook.subscribe.startup_once
+def start_once():
+    home = os.path.expanduser('~')
+    subprocess.call([home + '/.config/qtile/autostart.sh'])
+
+# XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
+# string besides java UI toolkits; you can see several discussions on the
+# mailing lists, GitHub issues, and other WM documentation that suggest setting
+# this string if your java app doesn't work correctly. We may as well just lie
+# and say that we're a working one by default.
+#
+# We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
+# java that happens to be on java's whitelist.
+wmname = "LG3D"
